2dcd25b0f43596300062cffee3bd32129e696eac
RomaaaK <zimpsonzya@gmail.com>
2022-01-07 00:17:18 +0300

    feat: implement function toReadable

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+    const numbers0_19 = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    const numbers20_90 = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+    if (number === 0) return 'zero';
+
+    const n = ('000' + number).slice(-3)
+
+    let res = ''
+
+    if (n[0] != 0) {
+        res += numbers0_19[n[0]] + ' hundred '
+    }
+    if (+n.slice(-2) < 20) {
+       res += numbers0_19[+`${number}`.slice(-2)]
+    } else if (+n.slice(-2) > 19) {
+        res += `${numbers20_90[n[1]]} ${numbers0_19[n[2]]}`
+    } else {
+        res += numbers0_19[n[2]]
+    }
+
+    return res.trim()
 }
